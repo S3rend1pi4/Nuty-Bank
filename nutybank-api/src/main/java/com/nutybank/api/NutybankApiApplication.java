1f0b6331ce8607526f015a5382570185580d80e2
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-21 15:31:04 +0100

    Created a nutybank api proyect

---
+++ nutybank-api/src/main/java/com/nutybank/api/NutybankApiApplication.java
@@ -0,0 +1,13 @@
+package com.nutybank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutybankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutybankApiApplication.class, args);
+    }
+
+}
