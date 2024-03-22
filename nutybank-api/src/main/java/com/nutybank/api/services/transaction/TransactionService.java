02cae8d485e7714a1420820208c122f5794250ed
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-22 15:29:27 +0100

    Added simple methods without logic and Javadoc on TransactionService

--- nutybank-api/src/main/java/com/nutybank/api/services/transaction/TransactionService.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/transaction/TransactionService.java
@@ -1,4 +1,56 @@
 package com.nutybank.api.services.transaction;
 
+import com.nutybank.api.entities.Transaction;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Esta interfaz define un servicio para gestionar transacciones.
+ */
 public interface TransactionService {
+
+    /**
+     * Busca una transacción por el nombre del cliente asociado.
+     *
+     * @param clientName El nombre del cliente asociado con la transacción a buscar.
+     * @return Un {@link Optional} que contiene la transacción encontrada,
+     * o un {@link Optional} vacío si no se encuentra ninguna transacción para el nombre de cliente dado.
+     */
+    Optional<Transaction> findByClientName(String clientName);
+
+    /**
+     * Busca una transacción por el DNI del cliente asociado.
+     *
+     * @param dni El DNI del cliente asociado con la transacción a buscar.
+     * @return Un {@link Optional} que contiene la transacción encontrada,
+     * o un {@link Optional} vacío si no se encuentra ninguna transacción para el nombre de cliente dado.
+     */
+    Optional<Transaction> findByClientDni(String dni);
+
+    /**
+     * Busca transacciones realizadas en una fecha específica.
+     *
+     * @param date La fecha en la que se realizaron las transacciones a buscar.
+     * @return Una lista de transacciones realizadas en la fecha especificada.
+     */
+    List<Transaction> findByDate(Date date);
+
+    /**
+     * Guarda una nueva transacción en la base de datos.
+     *
+     * @param transaction la transacción a guardar.
+     * @return La transacción guardada.
+     */
+    Transaction save(Transaction transaction);
+
+    /**
+     * Elimina una transacción de la base de datos.
+     *
+     * @param id El ID de la transacción a eliminar.
+     * @return Un {@link Optional} que contiene la transacción eliminada,
+     * o un {@link Optional} vacío si no encuentra ninguna transacción con el ID de transacción dado.
+     */
+    Optional<Transaction> delete(Long id);
 }
--- nutybank-api/src/main/java/com/nutybank/api/services/transaction/TransactionServiceImpl.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/transaction/TransactionServiceImpl.java
@@ -1,9 +1,38 @@
 package com.nutybank.api.services.transaction;
 
+import com.nutybank.api.entities.Transaction;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
 
+    @Override
+    public Optional<Transaction> findByClientName(String clientName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Transaction> findByClientDni(String dni) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Transaction> findByDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public Transaction save(Transaction transaction) {
+        return null;
+    }
+
+    @Override
+    public Optional<Transaction> delete(Long id) {
+        return Optional.empty();
+    }
 }
