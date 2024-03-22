61a9206ee38db8443ece7920434928333eea4e75
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-22 18:41:04 +0100

    Added logic simple methods on RoleService

--- nutybank-api/src/main/java/com/nutybank/api/services/role/RoleServiceImpl.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/role/RoleServiceImpl.java
@@ -1,21 +1,29 @@
 package com.nutybank.api.services.role;
 
+import com.nutybank.api.Repositories.RoleRepository;
 import com.nutybank.api.entities.Role;
+import com.nutybank.api.entities.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    @Autowired
+    private RoleRepository roleRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<Role> findById(Long id) {
-        return Optional.empty();
+        return roleRepository.findById(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<Role> findByName(String name) {
-        return Optional.empty();
+        return roleRepository.findByName(name);
     }
 }
