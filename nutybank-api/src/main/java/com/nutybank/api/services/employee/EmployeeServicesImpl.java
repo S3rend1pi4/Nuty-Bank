43e11c911620de08ba88342ad6b53b9cdd22c546
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-22 14:46:20 +0100

    Fix simple methods implementation

--- nutybank-api/src/main/java/com/nutybank/api/services/employee/EmployeeServicesImpl.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/employee/EmployeeServicesImpl.java
@@ -1,18 +1,15 @@
 package com.nutybank.api.services.employee;
 
-import com.nutybank.api.entities.Account;
-import com.nutybank.api.entities.Client;
 import com.nutybank.api.entities.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class EmployeeServicesImpl implements EmployeeService {
-    
+
     @Override
     public Page<Employee> findAll(Pageable pageable) {
         return null;
