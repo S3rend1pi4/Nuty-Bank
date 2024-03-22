e00c17d216593a4dc7e7e267de1ce7e5f03d45e8
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-22 14:45:11 +0100

    Added simple methods without logic and Javadoc on RoleService

--- nutybank-api/src/main/java/com/nutybank/api/services/role/RoleService.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/role/RoleService.java
@@ -1,4 +1,29 @@
 package com.nutybank.api.services.role;
 
+import com.nutybank.api.entities.Role;
+
+import java.util.Optional;
+
+/**
+ * Esta interfaz define un servicio para gestionar Roles
+ */
 public interface RoleService {
+
+    /**
+     * Busca un rol por su ID.
+     * ç
+     * @param id El id del rol a buscar.
+     * @return Un {@link Optional} que contiene el rol encontrado,
+     * o un {@link Optional} vacío si no se encuentra ningún rol con el ID dado.
+     */
+    Optional<Role> findById(Long id);
+
+    /**
+     * Busca un rol por su nombre.
+     *
+     * @param name El nombre del rol a buscar.
+     * @return Un {@link Optional} que contiene el rol encontrado,
+     * o un {@link Optional} vacío si no se encuentra ningún rol con el nombre dado
+     */
+    Optional<Role> findByName(String name);
 }
--- nutybank-api/src/main/java/com/nutybank/api/services/role/RoleServiceImpl.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/role/RoleServiceImpl.java
@@ -1,9 +1,21 @@
 package com.nutybank.api.services.role;
 
+import com.nutybank.api.entities.Role;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
 
+    @Override
+    public Optional<Role> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Role> findByName(String name) {
+        return Optional.empty();
+    }
 }
