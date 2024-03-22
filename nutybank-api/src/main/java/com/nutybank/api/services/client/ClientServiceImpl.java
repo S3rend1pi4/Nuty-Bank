cca4ce3780ea6ca5dd6246e15c4ab2d41d907759
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-22 14:03:19 +0100

    Added simple mehtods without logic and Javadoc on ClientService

--- nutybank-api/src/main/java/com/nutybank/api/services/client/ClientServiceImpl.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/client/ClientServiceImpl.java
@@ -1,9 +1,48 @@
 package com.nutybank.api.services.client;
 
+import com.nutybank.api.entities.Account;
+import com.nutybank.api.entities.Client;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientServiceImpl implements ClientService {
 
 
+    @Override
+    public Optional<Client> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Client> findeByUserName(String userName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Client> findByDni(String dni) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Account> findAllClientAccounts(Long userId) {
+        return null;
+    }
+
+    @Override
+    public Client save(Client client) {
+        return null;
+    }
+
+    @Override
+    public Optional<Client> update(Long id, Client client) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Client> delete(Long id) {
+        return Optional.empty();
+    }
 }
