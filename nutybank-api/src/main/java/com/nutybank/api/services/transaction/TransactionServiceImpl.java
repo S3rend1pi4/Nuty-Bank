fd75cf2fdd97ae59bc5c43a3560a9e12d50cb4ec
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-25 16:30:57 +0100

    Added logic simple methods on TransactionService

--- nutybank-api/src/main/java/com/nutybank/api/services/transaction/TransactionServiceImpl.java
+++ nutybank-api/src/main/java/com/nutybank/api/services/transaction/TransactionServiceImpl.java
@@ -1,8 +1,11 @@
 package com.nutybank.api.services.transaction;
 
+import com.nutybank.api.Repositories.TransactionRepository;
 import com.nutybank.api.entities.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -10,29 +13,43 @@ import java.util.Optional;
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Override
     public Optional<Transaction> findByClientName(String clientName) {
-        return Optional.empty();
+
+        return transactionRepository.findByClientTransactionName(clientName);
     }
 
     @Override
     public Optional<Transaction> findByClientDni(String dni) {
-        return Optional.empty();
+        return transactionRepository.findByClientTransactionDni(dni);
     }
 
     @Override
     public List<Transaction> findByDate(Date date) {
-        return null;
+        List<Transaction> allTransactions = transactionRepository.findAll();
+
+        List<Transaction> transactionByDate = new ArrayList<>();
+
+        for(Transaction transaction : allTransactions) {
+            if(transaction.getDate().equals(date)) {
+                transactionByDate.add(transaction);
+            }
+        }
+        return transactionByDate;
     }
 
     @Override
     public Transaction save(Transaction transaction) {
-        return null;
+        return transactionRepository.save(transaction);
     }
 
     @Override
     public Optional<Transaction> delete(Long id) {
-        return Optional.empty();
+        Optional<Transaction> transactionOptional = transactionRepository.findById(id);
+        transactionOptional.ifPresent(transaction -> transactionRepository.delete(transaction));
+        return transactionOptional;
     }
 }
