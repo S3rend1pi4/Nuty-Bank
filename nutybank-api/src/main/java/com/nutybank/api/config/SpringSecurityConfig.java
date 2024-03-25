8d1a512cc1a185ff086d78baaee851922775b34b
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-25 20:37:12 +0100

    Removed temporally springSecurity

--- nutybank-api/src/main/java/com/nutybank/api/config/SpringSecurityConfig.java
+++ nutybank-api/src/main/java/com/nutybank/api/config/SpringSecurityConfig.java
@@ -1,9 +1,8 @@
 package com.nutybank.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -13,4 +12,4 @@ public class SpringSecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
+}*/
