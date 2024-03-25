c5e55e12d1605951d4eb699fc736928a141286a8
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-03-25 20:36:33 +0100

    Added create account for existing user on controller

--- nutybank-api/src/main/java/com/nutybank/api/controllers/AccountController.java
+++ nutybank-api/src/main/java/com/nutybank/api/controllers/AccountController.java
@@ -1,9 +1,27 @@
 package com.nutybank.api.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.nutybank.api.entities.Account;
+import com.nutybank.api.services.account.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/accounts")
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
 
+    @PostMapping("/account/{id}")
+    public ResponseEntity<?> create(@PathVariable Long id, @Valid @RequestBody Account account) {
+
+        Account newAccount = accountService.openAccount(id, account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newAccount);
+
+    }
 }
