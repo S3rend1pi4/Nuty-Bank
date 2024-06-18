47b19ada4526b1b300256511b8d3da0096df2e5c
Serendipia <50060666+S3rend1pi4@users.noreply.github.com>
2024-06-18 22:10:33 +0200

    Added simple filterChain

--- nutybank-api/src/main/java/com/nutybank/api/config/SpringSecurityConfig.java
+++ nutybank-api/src/main/java/com/nutybank/api/config/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.nutybank.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -12,4 +15,14 @@ public class SpringSecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}*/
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/clients").permitAll()
+                .anyRequest().authenticated())
+                .csrf(config -> config.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
